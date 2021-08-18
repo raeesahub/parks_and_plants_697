93e67a0ca89ad7bd33c01f5f2bdfbb80529e73a8
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-08-18 18:05:34 +0100

    user can add a new plant

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :gardens do
+    resources :plants, only: [ :create ]
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+if Rails.env.development?
+	puts "Destroying gardens..."
+	Garden.destroy_all
+end
+
+puts "Creating gardens..."
+Garden.create!(
+  name: "My Little Garden",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg"
+)
+
+Garden.create!(
+  name: "My Other Garden",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
+)
+
+puts "Finished seeding!"
